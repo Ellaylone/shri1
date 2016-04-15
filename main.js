18ebdae885bfb452fb8571cd08b32d382137bd76
Ellaylone <heamikallove@gmail.com>
2016-04-15 15:37:20 +0000

    basic modal js

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
+var Modal = function(elem, confirm, toggle){
+    this.elem = elem;
+    this.overlay = document.getElementById("overlay");
+    var that = this;
+    this.elem.getElementsByClassName("modal__close")[0].addEventListener(
+        "click",
+        function(){ that.hide(); },
+        false
+    );
+    this.elem.getElementsByClassName("modal__confirm")[0].addEventListener(
+        "click",
+        function(){ if(confirm()){ that.hide(); } },
+        false
+    );
+    if(typeof toggle !== 'undefined'){
+        toggle.addEventListener("click", function(){ that.show(); }, false);
+    }
+}
 
+Modal.prototype.show = function(){
+    this.overlay.classList.remove("hidden");
+    this.elem.classList.remove("hidden");
+    return this;
+}
+
+Modal.prototype.hide = function(){
+    this.overlay.classList.add("hidden");
+    this.elem.classList.add("hidden");
+    return this;
+}
+var modalList = [];
+var channelList = new Modal(
+    document.getElementById("channellistModal"),
+    function(){
+        //TODO channel confirm
+        return true;
+    },
+    document.getElementById("channelSelect")
+);
+modalList.push(channelList);
+document.getElementById("overlay").addEventListener("click", function(){
+    modalList.forEach(function(modal){
+        modal.hide();
+    });
+}, false);
